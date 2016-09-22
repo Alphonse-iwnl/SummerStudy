a826a7a7d450d32630ab8a04aed15c2ae4803079
Evan <z1479763604@hotmail.com>
2016-09-22 23:34:28 +0800

    WPF自定义窗体的各种实现

---
+++ HomeWorkDemo/HomeWorkDemo/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeWorkDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Item> list = new List<Item>()
+        {
+            new Item {text="Study",imgSource="/Assets/Study.png" },
+            new Item {text="HomeWork",imgSource="/Assets/Homework.png"},
+            new Item {text="Community" ,imgSource="/Assets/Community.png" },
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            ItemList.ItemsSource = list;
+        }
+        public class Item
+        {  
+            public string text { get; set; }
+            public string imgSource { get; set; }
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+         
+
+        private void minButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; //设置窗口最小化
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        int i = 0;//判断点击次数
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            i += 1;
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);//300ms内双击
+            timer.Tick += (s, e1) => { timer.IsEnabled = false; i = 0; };
+            timer.IsEnabled = true;
+
+            if (i % 2 == 0)
+            {
+                timer.IsEnabled = false;
+                i = 0;
+                this.WindowState = this.WindowState == WindowState.Maximized ?
+                              WindowState.Normal : WindowState.Maximized;
+            }          
+        }
+    }
+}
